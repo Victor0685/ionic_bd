335fcc8d485782d888142543c99e680ef2e6782c
DevOtaviano <otavianovenancio24@gmail.com>
2024-05-31 09:31:08 -0300

    up

---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id: number
+  name: string
+  active: number
+  city_2: string
+  email: string
+}
---
+++ src/app/upgrades/user.upgrade.statements.ts
@@ -0,0 +1,27 @@
+export class UserUpgradeStatements {
+  userUpgrades = [
+      {
+      toVersion: 1,
+      statements: [
+          `CREATE TABLE IF NOT EXISTS users(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name TEXT NOT NULL,
+          active INTEGER DEFAULT 1
+          );`
+      ]
+      },
+      {
+      toVersion: 2,
+      statements: [
+          `ALTER TABLE users ADD COLUMN city_2 TEXT;`,
+      ]
+      },
+      {
+        toVersion: 3,
+        statements: [
+            `ALTER TABLE users ADD COLUMN email TEXT;`,
+        ]
+        },
+
+  ]
+}
